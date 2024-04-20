88fbef4dc8f86e3df0a270506a97788a36c612af
Sokolov Lev Sergeevich <89748410+levs16@users.noreply.github.com>
2024-04-21 02:36:31 +0300

    Add files via upload

---
+++ aitry-cuda.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import pickle
+
+# Load the text
+with open('large_text_file.txt', 'r') as file:
+    text = file.read()
+
+# Tokenize the text
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([text])
+total_words = len(tokenizer.word_index) + 1
+
+# Convert text to sequences of tokens
+input_sequences = []
+for line in text.split('\n'):
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# Pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+# Create predictors and label
+X, labels = input_sequences[:,:-1],input_sequences[:,-1]
+Y = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+# Define the model
+model = Sequential([
+    Embedding(total_words, 64, input_length=max_sequence_len-1),
+    LSTM(100),
+    Dense(total_words, activation='softmax')
+])
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Check for NVIDIA CUDA support
+if tf.test.is_built_with_cuda():
+    print("CUDA support detected. Using GPU for training.")
+    with tf.device('/GPU:0'):
+        model.fit(X, Y, epochs=100, verbose=1)
+else:
+    print("CUDA support not detected. Using CPU for training.")
+    model.fit(X, Y, epochs=100, verbose=1)
+
+# Save the model
+model.save('degro-hitler.h5')
+
+# Save the tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Function to generate text
+def generate_text(seed_text, next_words=3):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+        predictions = model.predict(token_list, verbose=0)
+        predicted = np.argmax(predictions, axis=-1)
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+    return seed_text
+
+# Generate text
+print(generate_text("Once upon a time"))
